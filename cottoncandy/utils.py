a7dfc11b324efca5d055506ef8f0ab9dea8ac3eb
Anwar Nunez-Elizalde <anwarnunez@gmail.com>
2016-10-30 15:58:54 -0700

    BF: browser fixed. closes #19

--- cottoncandy/utils.py
+++ cottoncandy/utils.py
@@ -156,7 +156,8 @@ def clean_object_name(input_function):
     '''
     @wraps(input_function)
     def iremove_root(self, object_name, *args, **kwargs):
-        object_name = os.path.normpath(object_name)
+        object_name = re.sub('//+', '/', object_name)
+
         if object_name == '':
             pass
         elif object_name[0] == SEPARATOR:
