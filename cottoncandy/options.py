b986aee304d81e94a67b0eecaec63cfc2bfc810f
Tianjiao Zhang <t.zhang@berkeley.edu>
2017-10-20 21:09:01 -0700

    stupid spaces.

--- cottoncandy/options.py
+++ cottoncandy/options.py
@@ -2,59 +2,59 @@ import os
 from base64 import b64decode, b64encode
 import sys
 try:
-	import configparser
+    import configparser
 except ImportError:
-	import ConfigParser as configparser
+    import ConfigParser as configparser
 from . import appdirs
 
 def get_key_from_s3fs():
-	'''If user has s3fs-fuse keys,return them
-	'''
-	key_path = os.path.expanduser('~/.passwd-s3fs')
-	if os.path.exists(key_path):
-		with open(key_path, 'r') as kfl:
-			content = kfl.readline()
-			ACCESS_KEY, SECRET_KEY = content.strip().split(':')
-			return ACCESS_KEY, SECRET_KEY
+    '''If user has s3fs-fuse keys,return them
+    '''
+    key_path = os.path.expanduser('~/.passwd-s3fs')
+    if os.path.exists(key_path):
+        with open(key_path, 'r') as kfl:
+            content = kfl.readline()
+            ACCESS_KEY, SECRET_KEY = content.strip().split(':')
+            return ACCESS_KEY, SECRET_KEY
 
 
 def get_key_from_environ():
-	try:
-		ak = os.environ['AWS_ACCESS_KEY'],
-		sk = os.environ['AWS_SECRET_KEY']
-		return ak, sk
-	except:
-		return
+    try:
+        ak = os.environ['AWS_ACCESS_KEY'],
+        sk = os.environ['AWS_SECRET_KEY']
+        return ak, sk
+    except:
+        return
 
 
 def get_keys():
-	'''try to find the user keys in the machine
-	'''
-	# try to outload keys
-	resulta = get_key_from_s3fs()
-	resultb = get_key_from_environ()
-	result = resultb if (resulta is None) else resulta
-	return result
+    '''try to find the user keys in the machine
+    '''
+    # try to outload keys
+    resulta = get_key_from_s3fs()
+    resultb = get_key_from_environ()
+    result = resultb if (resulta is None) else resulta
+    return result
 
 def generate_AES_key(bytes = 32):
-	"""Generates a new AES key
+    """Generates a new AES key
 
-	Parameters
-	----------
-	bytes : int
-		number of bytes in key
+    Parameters
+    ----------
+    bytes : int
+        number of bytes in key
 
-	Returns
-	-------
-	key : bytes
-	"""
-	try:
-		from Crypto import Random
-		return Random.get_random_bytes(bytes)
-	except ImportError:
-		print('PyCrypto not installed. Reading from /dev/random instead')
-		with open('/dev/random', 'r') as rand:
-			return rand.read(bytes)
+    Returns
+    -------
+    key : bytes
+    """
+    try:
+        from Crypto import Random
+        return Random.get_random_bytes(bytes)
+    except ImportError:
+        print('PyCrypto not installed. Reading from /dev/random instead')
+        with open('/dev/random', 'r') as rand:
+            return rand.read(bytes)
 
 cwd = os.path.split(os.path.abspath(__file__))[0]
 userdir = appdirs.user_data_dir("cottoncandy", "aone")
@@ -67,67 +67,67 @@ config.readfp(open(os.path.join(cwd, 'defaults.cfg')))
 
 # case no user config file
 if len(config.read(usercfg)) == 0:
-	if not os.path.exists(userdir):
-		os.makedirs(userdir)
+    if not os.path.exists(userdir):
+        os.makedirs(userdir)
 
-	# write keys to user config file
-	ak = config.get("login", "access_key")
-	sk = config.get("login", "secret_key")
-	if (ak == 'auto') and (sk == 'auto'):
-		result = get_keys()
-		if result is not None:
-			ak, sk = result
-		else:
-			ak = sk = 'KEYSNOTFOUND'
-		config.set("login", "access_key", ak)
-		config.set("login", "secret_key", sk)
+    # write keys to user config file
+    ak = config.get("login", "access_key")
+    sk = config.get("login", "secret_key")
+    if (ak == 'auto') and (sk == 'auto'):
+        result = get_keys()
+        if result is not None:
+            ak, sk = result
+        else:
+            ak = sk = 'KEYSNOTFOUND'
+        config.set("login", "access_key", ak)
+        config.set("login", "secret_key", sk)
 
-	aesKey = config.get('encryption', 'key')
-	if aesKey == 'auto':
-		newKey = generate_AES_key()
-		aesKey = str(b64encode(newKey))
-		with open(os.path.join(userdir, 'cloud-encryption-key.txt'), 'w') as keyfile:
-			keyfile.write(aesKey)
-		config.set("encryption", 'key', 'cloud-encryption-key.txt')
+    aesKey = config.get('encryption', 'key')
+    if aesKey == 'auto':
+        newKey = generate_AES_key()
+        aesKey = str(b64encode(newKey))
+        with open(os.path.join(userdir, 'cloud-encryption-key.txt'), 'w') as keyfile:
+            keyfile.write(aesKey)
+        config.set("encryption", 'key', 'cloud-encryption-key.txt')
 
-	with open(usercfg, 'w') as fp:
-		config.write(fp)
+    with open(usercfg, 'w') as fp:
+        config.write(fp)
 
 # add things to old versions of config if needed
 else:
-	needs_update = False
-	try:	# encryption section
-		aesKey = config.get('encryption', 'key')
-		if aesKey == 'auto':				# needs to generate a new key
-			aesKey = str(b64encode(generate_AES_key()))
-			with open(os.path.join(userdir, 'cloud-encryption-key.txt'), 'w') as keyfile:
-				keyfile.write(aesKey)
-			config.set("encryption", 'key', 'cloud-encryption-key.txt')
-			needs_update = True
-		elif aesKey[-4:] != '.txt':			# key is still directly stored in cottoncandy config
-			with open(os.path.join(userdir, 'cloud-encryption-key.txt'), 'w') as keyfile:
-				keyfile.write(aesKey)
-			config.set("encryption", 'key', 'cloud-encryption-key.txt')
-			needs_update = True
-	except configparser.NoSectionError:		# old config w/o encryption capabilities
-		config.add_section('encryption')
-		newKey = generate_AES_key()
-		aesKey = str(b64encode(newKey))
-		with open(os.path.join(userdir, 'cloud-encryption-key.txt'), 'w') as keyfile:
-			keyfile.write(aesKey)
-		config.set("encryption", 'key', 'cloud-encryption-key.txt')
-		config.set('encryption', 'method', 'AES')
-		needs_update = True
+    needs_update = False
+    try:	# encryption section
+        aesKey = config.get('encryption', 'key')
+        if aesKey == 'auto':				# needs to generate a new key
+            aesKey = str(b64encode(generate_AES_key()))
+            with open(os.path.join(userdir, 'cloud-encryption-key.txt'), 'w') as keyfile:
+                keyfile.write(aesKey)
+            config.set("encryption", 'key', 'cloud-encryption-key.txt')
+            needs_update = True
+        elif aesKey[-4:] != '.txt':			# key is still directly stored in cottoncandy config
+            with open(os.path.join(userdir, 'cloud-encryption-key.txt'), 'w') as keyfile:
+                keyfile.write(aesKey)
+            config.set("encryption", 'key', 'cloud-encryption-key.txt')
+            needs_update = True
+    except configparser.NoSectionError:		# old config w/o encryption capabilities
+        config.add_section('encryption')
+        newKey = generate_AES_key()
+        aesKey = str(b64encode(newKey))
+        with open(os.path.join(userdir, 'cloud-encryption-key.txt'), 'w') as keyfile:
+            keyfile.write(aesKey)
+        config.set("encryption", 'key', 'cloud-encryption-key.txt')
+        config.set('encryption', 'method', 'AES')
+        needs_update = True
 
-	try:	# gdrive section
-		secrets = config.get('gdrive', 'secrets')
-		credentials = config.get('gdrive', 'credentials')
-	except configparser.NoSectionError:
-		config.add_section('gdrive')
-		config.set('gdrive', 'secrets', 'client_secrets.json')
-		config.set('gdrive', 'credentials', 'credentials.txt')
-		needs_update = True
+    try:	# gdrive section
+        secrets = config.get('gdrive', 'secrets')
+        credentials = config.get('gdrive', 'credentials')
+    except configparser.NoSectionError:
+        config.add_section('gdrive')
+        config.set('gdrive', 'secrets', 'client_secrets.json')
+        config.set('gdrive', 'credentials', 'credentials.txt')
+        needs_update = True
 
-	if needs_update:
-		with open(usercfg, 'w') as configfile:
-			config.write(configfile)
+    if needs_update:
+        with open(usercfg, 'w') as configfile:
+            config.write(configfile)
