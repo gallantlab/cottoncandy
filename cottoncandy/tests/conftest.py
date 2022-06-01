a7b70adc6d40221d62001a84d3212604f7c25d3c
Tom Dupré la Tour <tom.dupre-la-tour@m4x.org>
2022-06-01 11:39:34 -0700

    longer wait time

--- cottoncandy/tests/conftest.py
+++ cottoncandy/tests/conftest.py
@@ -31,7 +31,7 @@ def cci(request):
             backend="s3",
             verbose=False,
         )
-        cci.wait_time = 0.5  # Account for Wasabi lag by waiting N [seconds]
+        cci.wait_time = 2.0  # Account for Wasabi lag by waiting N [seconds]
         yield cci
 
     elif request.param == "local":
