fe1c8943de6cce53378610a544cde7e6b32d454e
Aditya Vaidya <kroq-gar78@users.noreply.github.com>
2025-02-07 16:55:49 -0600

    DefaultInterface has the actual interface methods

--- cottoncandy/__init__.py
+++ cottoncandy/__init__.py
@@ -7,6 +7,8 @@ import os
 
 from cottoncandy import options
 
+from .browser import BrowserObject
+from .interfaces import DefaultInterface
 from .utils import get_keys, string2bool
 
 
@@ -29,7 +31,7 @@ def get_interface(bucket_name: str=default_bucket,
                   force_bucket_creation: bool=force_bucket_creation,
                   verbose: bool=True,
                   backend: Literal['s3', 'gdrive', 'local']='s3',
-                  **kwargs) -> InterfaceObject:
+                  **kwargs) -> DefaultInterface:
     """Return an interface to the cloud.
 
     Parameters
@@ -49,7 +51,7 @@ def get_interface(bucket_name: str=default_bucket,
 
     Returns
     -------
-    cci : cottoncandy.InterfaceObject
+    cci : cottoncandy.DefaultInterface
     """
     from cottoncandy.interfaces import DefaultInterface
 
