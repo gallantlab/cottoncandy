8c6856e2cd977d89d46844075e70eef62689f7ce
Aditya Vaidya <kroq-gar78@users.noreply.github.com>
2026-02-23 02:56:41 -0800

    Fix localclient.move return type

--- cottoncandy/localclient.py
+++ cottoncandy/localclient.py
@@ -224,7 +224,7 @@ class LocalClient(CCBackEnd):
             shutil.copy(source_metadata, destination_metadata)
 
         auto_makedirs(destination)
-        return shutil.copy(source, destination)
+        shutil.copy(source, destination)
 
     def move(self, source: str, destination: str, source_bucket: Optional[str] = None, destination_bucket: Optional[str] = None, overwrite: bool = False) -> bool:
         """Moves an object
